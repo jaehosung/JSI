fa341dd5c41ed2c4da4aae52cfb6b4ad14256202
Jaeho Sung <jaehossung@gmail.com>
2022-03-21 22:56:20 +0900

    undo and redo

---
+++ JSI/Cmd/JSICmdToSelectSmallestStandingCardByStand.cs
@@ -0,0 +1,40 @@
+﻿using X;
+using JSI.Scenario;
+
+namespace JSI.Cmd {
+    public class JSICmdToSelectSmallestStandingCardByStand :
+        XLoggableCmd {
+        // fields
+        JSIStandingCard mSelectedStandingCard = null;
+
+        // private constructor
+        private JSICmdToSelectSmallestStandingCardByStand(XApp app) :
+            base(app) {
+            JSIApp jsi = (JSIApp)this.mApp;
+        }
+
+        // static method to construct and execute this command
+        public static bool execute(XApp app) {
+            JSICmdToSelectSmallestStandingCardByStand cmd =
+                new JSICmdToSelectSmallestStandingCardByStand(app);
+            return cmd.execute();
+        }
+
+        protected override bool defineCmd() {
+            JSIApp jsi = (JSIApp)this.mApp;
+            JSIEditStandingCardScenario scenario =
+                JSIEditStandingCardScenario.getSingleton();
+            this.mSelectedStandingCard = scenario.selectStandingCardByStand();
+            scenario.setSelectedStandingCard(this.mSelectedStandingCard);
+            return true;
+        }
+
+        protected override XJson createLogData() {
+            XJson data = new XJson();
+            JSIStandingCard sc = JSIEditStandingCardScenario.getSingleton().
+                getSelectedStandingCard();
+            data.addMember("cardId", sc.getId());
+            return data;
+        }
+    }
+}
